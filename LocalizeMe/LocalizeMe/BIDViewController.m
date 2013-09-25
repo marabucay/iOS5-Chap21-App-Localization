46a2363305f2f205b6d8aa3f1bc4e29559abe05d
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-25 13:05:29 +0800

    Implemented the View Controller.

--- LocalizeMe/LocalizeMe/BIDViewController.m
+++ LocalizeMe/LocalizeMe/BIDViewController.m
@@ -9,6 +9,12 @@
 #import "BIDViewController.h"
 
 @implementation BIDViewController
+@synthesize localeLabel;
+@synthesize label1;
+@synthesize label2;
+@synthesize label3;
+@synthesize label4;
+@synthesize label5;
 
 - (void)didReceiveMemoryWarning
 {
@@ -22,6 +28,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLocale *locale = [NSLocale currentLocale];
+    NSString *displayNameString = [locale
+                                   displayNameForKey:NSLocaleIdentifier
+                                   value:[locale localeIdentifier]];
+    localeLabel.text = displayNameString;
+    label1.text = NSLocalizedString(@"One", @"The number 1");
+    label2.text = NSLocalizedString(@"Two", @"The number 2");
+    label3.text = NSLocalizedString(@"Three", @"The number 3");
+    label4.text = NSLocalizedString(@"Four", @"The number 4");
+    label5.text = NSLocalizedString(@"Five", @"The number 5");
 }
 
 - (void)viewDidUnload
@@ -29,6 +45,12 @@
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
+    self.localeLabel = nil;
+    self.label1 = nil;
+    self.label2 = nil;
+    self.label3 = nil;
+    self.label4 = nil;
+    self.label5 = nil;
 }
 
 - (void)viewWillAppear:(BOOL)animated
